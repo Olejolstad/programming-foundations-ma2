1db8036d32dbfd8ffbdabeb5d963bf0c2ad9d415
olejolstad <olejolstad@gmail.com>
2021-03-12 13:30:15 +0100

    Done assignment
    Complete

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// Question 1
+
+// var pets = [
+// 	{
+// 		type: "cat",
+// 		age: 5.5,
+// 	},
+// 	{
+// 		type: "dog",
+// 		age: 3.8,
+// 	},
+// 	{
+// 		type: "parrot",
+// 		age: 4.0,
+// 	},
+// ];
+
+
+
+// for (var i = 0; i < pets.length; i++) {
+//     var petType = pets[i].type;
+//     var age = pets[i].age;
+
+//     if (age >= 4) {
+//         console.log(petType)
+//     }
+// }
+
+
+// Question 2
+
+// function logged(anArgument) {
+//     var whatType = typeof anArgument;
+
+//     if (whatType !== "boolean") {
+//         return "Please pass a boolean value in";
+//     }
+
+//     return anArgument;
+// }
+
+// var isItBoolean = logged(true);
+// console.log(isItBoolean);
+
+
+// Question 3
+
+
+// var heading = document.querySelector("h2");
+// var button = document.querySelector("button");
+
+// function headingChanges () {
+//     heading.innerHTML = "Updated subheading";
+//     heading.style.color = "blue";
+// }
+
+// button.onclick = headingChanges
